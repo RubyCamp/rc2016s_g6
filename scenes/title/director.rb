cf10d247355981780a8a9e4611e5034a33cb5f35
Mika Kaneko <test@example.com>
2016-08-24 11:47:55 +0900

    タイトル画像を表示するようにした

--- scenes/title/director.rb
+++ scenes/title/director.rb
@@ -1,12 +1,12 @@
 module Title
 	class Director
 		def initialize
-#			@bg_img = Image.load("images/background_title.png")
+			@bg_img = Image.load("images/title.png")
 		end
 
 		def play
-#			Window.draw(0, 0, @bg_img)
-			if Input.keyPush?(K_RETURN)
+			Window.draw(0, 0, @bg_img)
+			if Input.keyPush?(K_SPACE)
 				Scene.set_current_scene(:play)
 			end
 		end
