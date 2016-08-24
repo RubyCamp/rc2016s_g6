d4e8caf505b8fd7c1276d39fd4cff86c6616efef
ToruNagi <u052ff@yamaguchi-u.ac.jp>
2016-08-24 09:58:47 +0900

    マップを表示できるように変更

--- scenes/play/director.rb
+++ scenes/play/director.rb
@@ -1,43 +1,43 @@
-=begin
-require_relative '../lib/Ginchaku'
-require_relative '../lib/map'
-require_relative '../lib/Obake'
-require_relative '../lib/Player'
-require_relative '../lib/Shame'
-require_relative '../lib/Takara'
-=end
+
+#require_relative '../lib/Ginchaku'
+require_relative '../../lib/map'
+#require_relative '../lib/Obake'
+#require_relative '../lib/Player'
+#require_relative '../lib/Shame'
+#require_relative '../lib/Takara'
+
 module Play
 	class Director
 
-		TIME_LIMIT = 10
+		TIME_LIMIT = 1000000
 
 		def initialize
 			@start_time = Time.now
 			@time_count = TIME_LIMIT
-			@render_target = Render_Target.new(800, 600)
-=begin
-			@map = Map.new(File.join(File.dirname(__FILE__), "..", "images", "map.dat"))	#訂正いるかな？
+			@render_target = RenderTarget.new(800, 600)
+
+			@map = Map.new("images/map.dat")	
 #			@info_window = InfoWindow.new()
 			@characters = []
 			@takaras = []
-			10.times do
-				point = [] #場所ランダム
-				if 
-					redo
-				end
-				@takaras << Takara.new()	#引数ううう
-			end
-			@characters += @takaras
+#			10.times do
+#				point = [] #場所ランダム
+#				if 
+#					redo
+#				end
+#				@takaras << Takara.new()	#引数ううう
+#			end
+#			@characters += @takaras
 			@enemies = []
-			@enemies << Ginchaku.new()	#引数ううう
-			2.times { @enemies << Shame.new() }	#引数ううう
-			@characters += @enemies
-			@player = Player.new
-			@characters << @player
-			@characters.each do |char|
-				char.target = @render_target
-			end
-=end
+#			@enemies << Ginchaku.new()	#引数ううう
+#			2.times { @enemies << Shame.new() }	#引数ううう
+#			@characters += @enemies
+#			@player = Player.new
+#			@characters << @player
+#			@characters.each do |char|
+#				char.target = @render_target
+#			end
+
 		end
 
 		def play
@@ -45,15 +45,15 @@ module Play
 				Scene.set_current_scene(:ending)
 			end
 			count_down
-=begin
-			Sprite.update(@characters)
-			Sprite.check(@enemies. @player)
-			Sprite.check(@player, @takaras)
-			compact
+
+#			Sprite.update(@characters)
+#			Sprite.check(@enemies. @player)
+#			Sprite.check(@player, @takaras)
+#			compact
 			@map.draw
 #			@info_window.draw
-			Sprite.draw(@characters)
-=end
+#			Sprite.draw(@characters)
+
 		end
 
 		private
