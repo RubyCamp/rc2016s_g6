ffd6ddfb63f12b755d9ded7c1acd2fb723e90e3e
ToruNagi <u052ff@yamaguchi-u.ac.jp>
2016-08-24 14:27:19 +0900

    awa.rbを修正

--- lib/awa.rb
+++ lib/awa.rb
@@ -2,10 +2,17 @@ class Awa < Sprite
 
 	#生成する座標が引数
 	def initialize(a, b)
-		self.imgae("images/awa.png")
+		self.image = Image.load('images/awa.png')
 		self.image.set_color_key(C_WHITE)
 		self.x, self.y = a, b
 	end
 
-	
+
+	#プレイヤーにぶつかったら泡が消えるようにする
+	def hit(obj)
+		if obj.is_a?(Player)
+			self.vanish
+		end
+	end
+
 end
