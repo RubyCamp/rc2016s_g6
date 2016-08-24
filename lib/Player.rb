93edae52832c92c2503f2018dd2ca3c72863c44f
hayabusa <test@example.com>
2016-08-24 09:13:22 +0900

    Player.rb 当たり判定追加

--- lib/Player.rb
+++ lib/Player.rb
@@ -2,33 +2,34 @@ class Player < Sprite
   attr_reader :life, :score
 
   def initialize(image = nil)
-    image = Image.load("images/player.png") #image_path("player.png"))
+    image = Image.load("images/player.png")
     image.set_color_key(C_WHITE)
-      @center_x = Window.width/2  - image.width/2
-      @center_y = Window.height/2 - image.height/2
+    @center_x = Window.width/2  - image.width/2
+    @center_y = Window.height/2 - image.height/2
     super(@center_x, @center_y, image)
-    @life = 3.0
+    @life = 100.0
     @score = 0
   end
 
   def update
-    @life -= 0.01
+    @life -= 0.1
     return if @life <= 0
     # map = Director.instance.map
     # dy = -1 if Input.key_push?(K_UP) && map.movable?(self.x, self.y-1)
     # dy = 1  if Input.key_push?(K_DOWN) && map.movable?(self.x, self.y+1)
     # dx = 1  if Input.key_push?(K_RIGHT) && map.movable?(self.x+1, self.y)
     # dx = -1 if Input.key_push?(K_LEFT) && map.movable?(self.x-1, self.y)
+    dx = 0
+    dy = -1                             #沈む
     dx = 3 if Input.key_down?(K_LEFT)
     dx = -3 if Input.key_down?(K_RIGHT)
-    dy = -1
     dy -= 3 if Input.key_down?(K_DOWN)
     dy += 3 if Input.key_down?(K_UP)
     move(dx, dy)
   end
 
   def move(dx,dy)
-    if dx
+    if (Window.ox + Window.width/2 - self.image.width/2 > 0 || dx < 0) && (Window.ox - Window.width*3/2 + self.image.width < 0 || dx > 0)
       Window.ox -= dx
       self.x = @center_x + Window.ox
     end
@@ -38,19 +39,21 @@ class Player < Sprite
     end
   end
 
-  # コインを取ったとき
-  def shot(obj)
-    @score += 1
-  end
-
-  # 敵に当たったとき
-  def attacked(obj)
-    @life -= 3
-    if @life < 1
-      vanish
-      return
+  # 当たり判定
+  def hit(obj)
+    if obj.is_a?(Same) #サメにあたったとき
+      @life -= 3
+      if @life <= 0
+        vanish
+        return
+      end
     end
-
+    if obj.is_a?(Awa) #泡をとったとき
+      life = 100
+    end
+    if obj.is_a?(Takara) #宝をとったとき
+      @score += 100
+    end
   end
 
   private
