19e42eb0225e6ff7033b82cde379b1a63ee381cb
Mika Kaneko <test@example.com>
2016-08-26 06:35:10 +0900

    生成場所の修正等　中身は大幅に変えたが動きは他に影響しない

--- lib/director.rb
+++ lib/director.rb
@@ -17,13 +17,15 @@ class Director
 	SAME = 5
 	AWA = 4
 	TAKARA = 15
+	GINCHAKU = 1
 	include Singleton
 	attr_reader :map, :same, :player, :time_count, :esacount
 
 	def initialize
-		@newflg = true
+		@new_flg = true
 	end
-	def newall
+	def new_all
+		@red = Image.new(800, 600, [255, 0, 0])
 		@font = Font.new(25)
 		@start_time = Time.now
 		@time_count = TIME_LIMIT
@@ -31,58 +33,33 @@ class Director
 		@render_target = RenderTarget.new(@map.width, @map.height)
 		@info_window = InfoWindow.new(@time_count)
 		@characters = []
-		pos = [-1, -1]
-		@map.map_x_size.times do |x|
-			@map.map_y_size.times do |y|
-				if @map.block(x, y) == 6
-					pos = [x * 32, y * 32 - 55]
-					break
-				end
-			end
-			if pos != [-1, -1]
-				break
-			end
-		end
-		if pos != [-1, -1]
-			@ship = Ship.new(pos[0], pos[1])
-			@ship.target = @render_target
-			@characters << @ship
-		end
+		ship_new
+		possible_new
 		@takaras = []
-		@possible = Array.new { Array.new(3) }
-		0.step(@map.map_x_size, 2) do |x|
-			(@map.map_y_size - 1).times do |y|
-				if @map.block(x, y) == 1
-					if @map.block(x, y - 1) == 0
-						@possible << [0, x * 32, (y - 2) * 32]
-					end
-				end
-			end
-		end
 	    TAKARA.times do
 			pos = setpos
 			@takaras << Takara.new(pos[0], pos[1])
 		end
 		@characters += @takaras
-		@enemies = []
-		@objects = []
-		@ghosts_pos_x = []
-		@ghosts_pos_y = []
-		@ghosts_count = []
-		@ghosts = []
-		pos = setpos
-		@objects << Ginchaku.new(pos[0], pos[1])
+		@ginchakus = []
+		GINCHAKU.times do
+			pos = setpos
+			@ginchakus << Ginchaku.new(pos[0], pos[1])
+		end
+		@characters += @ginchakus
+		@awas = []
 		AWA.times do
 			pos = setpos_ex(64, 64)
-			@objects << Awa.new(pos[0], pos[1])
+			@awas << Awa.new(pos[0], pos[1])
 		end
-		@characters += @objects
+		@characters += @awas
+		@sames = []
 		SAME.times do
 			pos = setpos_ex(92, 46)
 			redo if pos[1] < Window.height * 3 / 4
-			@enemies << Same.new(pos[0], pos[1])
+			@sames << Same.new(pos[0], pos[1])
 		end
-		@characters += @enemies
+		@characters += @sames
 		@player = Player.new
 		@characters << @player
 		@characters.each do |char|
@@ -90,14 +67,16 @@ class Director
 		end
 		@esas = []
 		@esacount = ESA_LIMIT
-
-		@red = Image.new(800, 600, [255, 0, 0])
+		@ghosts_pos_x = []
+		@ghosts_pos_y = []
+		@ghosts_count = []
+		@ghosts = []
 	end
 
 	def play
-		if @newflg
-			newall
-			@newflg = false
+		if @new_flg
+			new_all
+			@new_flg = false
 		end
 		count_down
 		if Input.keyPush?(K_X)
@@ -106,31 +85,12 @@ class Director
 				esa = Esa.new(@player.x, @player.y)
 				esa.target = @render_target
 				@esas << esa
-				@objects << esa
 				@characters << esa
 			end
 		end
 		Sprite.update(@characters)
 		Sprite.check(@characters, @characters)
-		@takaras.each do |takara|
-			if takara.vanished?
-				@ghosts_pos_x << takara.x
-				@ghosts_pos_y << takara.y
-				@ghosts_count << GHOST_APPEAR_TIME
-			end
-		end
-		@ghosts_count.each_with_index do |count, i|
-			@ghosts_count[i] -= 1 if count >= 0
-			if count == 0
-				x = @ghosts_pos_x[i]
-				y = @ghosts_pos_y[i]
-				@ghost = Ghost.new(x,y)
-				@ghost.target = @render_target
-				@ghosts << @ghost
-				@enemies << @ghost
-				@characters << @ghost
-			end
-		end
+		takara_to_obake
 		Sprite.clean(@characters)
 		Sprite.clean(@takaras)
 		@render_target.draw(0,0,@map.draw)
@@ -141,25 +101,38 @@ class Director
 		if game_over? || @ship.clear
 			Scene.set_current_scene(:ending)
 		end
-
-
-		Window.draw_alpha(0, 0, @red, (Player::LIFE - @player.life) / (Player::LIFE / 2))
+		Window.draw_alpha(0, 0, @red, (Player::LIFE - @player.life) / 2)
 	end
 
 	def pos_limit?(x,y,image_width, image_height)
 		x < 0 || x - image_width > @map.width || y < 0 || y - image_height > @map.height
 	end
 
+	def dxdy(size)
+		d = size % 32
+		if d == 0
+			d = 1
+		elsif d >= 2
+			d = 2
+		end
+		d += size / 32
+		return d
+	end
+
 	def setpos_ex(image_width, image_height)
 		begin
-			x = rand(@map.width)
-			y = rand(@map.height - 32)
+			x = rand(@map.width - image_width)
+			y = rand(@map.height - image_height)
 			block = false
-			4.times do |i|
-				3.times do |j|
-					if x + i * 32 < 0 || x + i * 32 > @map.width || y + j * 32 < 0 || y + j * 32 > @map.height
+			dx = dxdy(image_width)
+			dy = dxdy(image_height)
+			(image_width / 32 + dx).times do |i|
+				(image_height / 32 + dy).times do |j|
+					if x / 32 + i > @map.map_x_size - 1 || y / 32 + j > @map.map_y_size - 1
 						block = true
-    				elsif @map.block(x / 32 + i, y / 32 + j) != 0
+						next
+					end
+					if @map.block(x / 32 + i, y / 32 + j) != 0
 						block = true
 					end
 				end
@@ -182,13 +155,86 @@ class Director
 		end
 		return flg
 	end
+
 	def setpos
 		begin
 			point = rand(@possible.length)
 		end while @possible[point][0] == 1
-		@possible[point][0] = 1
 		x = @possible[point][1]
 		y = @possible[point][2]
+		d = dxdy(64)
+		2.times do |i|
+			2.times do |j|
+				while index = @possible.index([0, x - 32 + i * 64, y - 32 + j * 64]) do
+					@possible[index][0] = 1
+				end
+			end
+		end
 		return [x, y]
+	end
+
+	def ship_new
+		pos = [-1, -1]
+		@map.map_x_size.times do |x|
+			@map.map_y_size.times do |y|
+				if @map.block(x, y) == 6
+					pos = [x * 32, y * 32 - 55]
+					break
+				end
+			end
+			if pos != [-1, -1]
+				break
+			end
+		end
+		if pos != [-1, -1]
+			@ship = Ship.new(pos[0], pos[1])
+			@ship.target = @render_target
+			@characters << @ship
+		end
+	end
+
+	def possible_new
+		@possible = Array.new { Array.new(3) }
+		0.step(@map.map_x_size, 2) do |x|
+			(@map.map_y_size - 1).times do |y|
+				if x + 1 < @map.map_x_size && y - 2 >= 0 
+					if @map.block(x, y) == 1
+						if @map.block(x, y - 1) == 0 && @map.block(x + 1, y - 1) == 0
+							if map.block(x, y - 2) == 0 && @map.block(x + 1, y - 2) == 0
+								@possible << [0, x * 32, (y - 2) * 32]
+							end
+						end
+					elsif @map.block(x - 1, y) == 1
+						x -= 1
+						if @map.block(x, y - 1) == 0 && @map.block(x + 1, y - 1) == 0
+							if map.block(x, y - 2) == 0 && @map.block(x + 1, y - 2) == 0
+								@possible << [0, x * 32, (y - 2) * 32]
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+
+	def takara_to_obake
+		@takaras.each do |takara|
+			if takara.vanished?
+				@ghosts_pos_x << takara.x
+				@ghosts_pos_y << takara.y
+				@ghosts_count << GHOST_APPEAR_TIME
+			end
+		end
+		@ghosts_count.each_with_index do |count, i|
+			@ghosts_count[i] -= 1 if count >= 0
+			if count == 0
+				x = @ghosts_pos_x[i]
+				y = @ghosts_pos_y[i]
+				@ghost = Ghost.new(x,y)
+				@ghost.target = @render_target
+				@ghosts << @ghost
+				@characters << @ghost
+			end
+		end
 	end
 end
