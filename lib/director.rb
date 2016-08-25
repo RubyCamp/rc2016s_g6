1edc289d1ba8c71d929c9e91b2079ae35291f436
Mika Kaneko <test@example.com>
2016-08-25 09:33:07 +0900

    もう一回修正しました

--- lib/director.rb
+++ lib/director.rb
@@ -68,8 +68,8 @@ class Director
 
 	def play
 		count_down
-		if Input.key_push(K_X)
-		end
+#		if Input.keyPush(K_X)
+#		end
 		Sprite.update(@characters)
 		Sprite.check(@characters, @characters)
 		@takaras.each do |takara|
